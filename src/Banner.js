c91853f85da8ff6caf4be4e709fbe66d87ccafd7
Dayo Asaolu <asaoludavid@gmail.com>
2021-07-01 22:24:33 -0400

    first commit

---
+++ src/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+import React from 'react'
+
+function Banner() {
+    return (
+        <div>
+            <h1>I am the banner</h1>
+        </div>
+    )
+}
+
+export default Banner
